c83969608c2bd79e142d81e9ca81c9454c6810cd
Vittorio <tonibusatta@gmail.com>
2023-06-28 01:35:04 +0400

    changes

--- components/Landing/Landing.js
+++ components/Landing/Landing.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React, { useState } from "react";
+import React from "react";
 import landingCard from "../../public/images/1_bg.jpg";
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
