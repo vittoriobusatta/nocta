dce4470488af28707b76c6be2d7ae2d623f06b2b
Vittorio <tonibusatta@gmail.com>
2023-07-04 23:16:56 +0400

    fix cursor

--- components/Cursor.js
+++ components/Cursor.js
@@ -4,15 +4,17 @@ import React, { useEffect, useRef } from "react";
 function Cursor() {
   const cursorBig = useRef(null);
   const cursorSmall = useRef(null);
-  const links = document.querySelectorAll("a");
-  const buttons = document.querySelectorAll("button");
-  const withHover = [...links, ...buttons];
 
   useEffect(() => {
     const cursorBigRef = cursorBig.current;
     const cursorSmallRef = cursorSmall.current;
 
     if (!cursorBigRef || !cursorSmallRef) return;
+
+    const links = document.querySelectorAll("a");
+    const buttons = document.querySelectorAll("button");
+    const showimgs = document.querySelectorAll(".showcase__images");
+    const withHover = [...links, ...buttons, ...showimgs];
 
     function onMouseMove(e) {
       cursorSmallRef.style.opacity = 1;
@@ -31,7 +33,7 @@ function Cursor() {
     function onMouseHover() {
       gsap.to(cursorBigRef, {
         duration: 0.3,
-        scale: 3,
+        scale: 1.8,
       });
     }
 
@@ -53,6 +55,7 @@ function Cursor() {
       cursorBigRef.style.opacity = 0;
       cursorSmallRef.style.opacity = 0;
     });
+
     withHover.forEach((element) => {
       element.addEventListener("mouseover", onMouseHover);
       element.addEventListener("mouseout", onMouseHoverOut);
@@ -69,6 +72,11 @@ function Cursor() {
       document.removeEventListener("mouseleave", () => {
         cursorBigRef.style.opacity = 0;
         cursorSmallRef.style.opacity = 0;
+      });
+
+      withHover.forEach((element) => {
+        element.removeEventListener("mouseover", onMouseHover);
+        element.removeEventListener("mouseout", onMouseHoverOut);
       });
     };
   }, [cursorBig, cursorSmall]);
